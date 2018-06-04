d6a0b1775023387ef2e2c48c637b2b846687db90
yonghai.yang <yangyonghai@wusongtech.com>
2018-06-04 12:10:19 +0800

    传参数 :cat:

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(" Learning Java! June 4 2018 ");
+        System.out.println(" hello test! ");
+        System.out.println(args[1]);
     }
 }
 
