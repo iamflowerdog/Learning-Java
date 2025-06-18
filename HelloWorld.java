d1c0bb1b07501454bff10d7057a511551c54ea4b
yang.yang <yang.yang@polyvirtual.com>
2025-06-18 14:51:37 +0800

    朝花夕拾 java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(" Learning Java! June 4 2018 ");
-        System.out.println(" hello test! ");
         System.out.println(args[1]);
+
+        // This is a comment
+        // This is another comment
+        System.out.println(args[0] + " learning Java at June 18 2025");
     }
 }
 
