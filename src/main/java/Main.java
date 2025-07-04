25eedce6f2e7e7b62eaf37877fe7e16b0b0cd0fa
iamflowerdog <yangyonghai9125@hotmail.com>
2025-07-04 10:56:00 +0800

    更新 lanch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,5 +22,16 @@ public class Main {
         Cat myCat = new Cat("Tom", 3, "Gray");
         myCat.displayInfo();
         
+        // 演示私有化字段的访问限制
+        Dog testDog = new Dog("Max", 5, "Labrador");
+        
+        // 以下代码会导致编译错误，因为这些字段是私有的
+        // testDog.name = "Buddy";  // 错误：name 是私有的
+        // testDog.age = 6;         // 错误：age 是私有的
+        // testDog.breed = "Husky"; // 错误：breed 是私有的
+        
+        // 正确的方式是使用公共方法访问
+        testDog.setName("Buddy");   // 正确：通过公共方法修改名字
+        System.out.println("Dog's new name: " + testDog.getName());
     }
 }
